fe252da064fcda863b94fd92e976aab218cabb37
Saba Abbas <sabaabbas23@gmail.com>
2018-12-05 13:25:16 +0500

    initial commit

--- app/src/main/java/com/example/anumabbas/traveljournal/UserProfile.java
+++ app/src/main/java/com/example/anumabbas/traveljournal/UserProfile.java
@@ -1,8 +1,11 @@
 package com.example.anumabbas.traveljournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,32 +22,39 @@ public class UserProfile extends AppCompatActivity {
     EditText City;
     EditText Country;
     EditText Phone;
+    private DatabaseReference mDatabaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        mDatabaseRef= FirebaseDatabase.getInstance().getReference();
+        Toast.makeText(this, "Check!!", Toast.LENGTH_SHORT).show();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid= user.getUid();
+
         Name   = (EditText)findViewById(R.id.name);
         Age   = (EditText)findViewById(R.id.age);
         City   = (EditText)findViewById(R.id.city);
         Country   = (EditText)findViewById(R.id.country);
         Phone   = (EditText)findViewById(R.id.phone);
 
-        final String name= Name.getText().toString().trim();
-        final String age= Age.getText().toString().trim();
-        final String city= City.getText().toString().trim();
-        final String country= Country.getText().toString().trim();
-        final String phone= Phone.getText().toString().trim();
+        //final String name= Name.getText().toString().trim();
+        //final String age= Age.getText().toString().trim();
+        //final String city= City.getText().toString().trim();
+        //final String country= Country.getText().toString().trim();
+        //final String phone= Phone.getText().toString().trim();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String uid= user.getUid();
+
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference ref= database.getReference("Users/"+uid);
 
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Toast.makeText(getApplicationContext(), "On data changed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "On data changed", Toast.LENGTH_SHORT).show();
                 ProfileInfo pro = dataSnapshot.getValue(ProfileInfo.class);
 
                 Name.setText(pro.getName().trim());
@@ -63,5 +73,47 @@ public class UserProfile extends AppCompatActivity {
 
 
 
+    }
+
+
+
+
+    public void UpdateInformation(View view) {
+
+
+
+        Name   = (EditText)findViewById(R.id.name);
+        Age   = (EditText)findViewById(R.id.age);
+        City   = (EditText)findViewById(R.id.city);
+        Country   = (EditText)findViewById(R.id.country);
+        Phone   = (EditText)findViewById(R.id.phone);
+
+        final String name= Name.getText().toString().trim();
+        final String age= Age.getText().toString().trim();
+        final String city= City.getText().toString().trim();
+        final String country= Country.getText().toString().trim();
+        final String phone= Phone.getText().toString().trim();
+
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid=user.getUid();
+
+        DatabaseReference ref= mDatabaseRef.child("Users").child(uid);
+        String key=ref.push().getKey();
+        ProfileInfo prof=new ProfileInfo(name,age,city,country,phone,uid,key);
+
+
+        ref.setValue(prof);
+        Toast.makeText(this, "Updated Profile Successfully!!", Toast.LENGTH_SHORT).show();
+
+        Intent i = new Intent(getApplicationContext(),Home.class);
+        finish();
+        startActivity(i);
+    }
+
+    public void goToUserProfile(View view) {
+        Intent i = new Intent(getApplicationContext(), UserProfile.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/anumabbas/traveljournal/WishList.java
+++ app/src/main/java/com/example/anumabbas/traveljournal/WishList.java
@@ -8,6 +8,6 @@ public class WishList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wish_list);
+        setContentView(R.layout.activity_upload_media);
     }
 }
